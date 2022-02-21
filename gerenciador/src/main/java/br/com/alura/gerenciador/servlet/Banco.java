3237f0b75f097e192c347ca8527c13e8067c4cfa
Niltemberg <niltemberg@gmail.com>
2022-02-20 21:35:05 -0300

    Curso Servlet finalizado

--- gerenciador/src/main/java/br/com/alura/gerenciador/servlet/Banco.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/servlet/Banco.java
@@ -1,16 +1,20 @@
 package br.com.alura.gerenciador.servlet;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Banco {
 	
 	private static List<Empresa> lista = new ArrayList<Empresa>();
+	private static Integer chaveSequencial = 1;
 
 	static {
 		Empresa empresa = new Empresa();
+		empresa.setId(chaveSequencial++);
 		empresa.setNome("ArtEmLibras");
 		Empresa empresa1 = new Empresa();
+		empresa1.setId(chaveSequencial++);
 		empresa1.setNome("Unidiversidade");
 		lista.add(empresa1);
 		lista.add(empresa);
@@ -18,11 +22,38 @@ public class Banco {
 	
 	public void adiciona(Empresa empresa) {
 		// TODO Auto-generated method stub
+		empresa.setId(Banco.chaveSequencial++);
 		Banco.lista.add(empresa);
 	}
 	
 	public List<Empresa> getEmpresas(){
 		return Banco.lista;
+	}
+
+	public void removeEmpresa(Integer id) {
+		
+		Iterator<Empresa> it = lista.iterator();
+		
+		while(it.hasNext()) {
+			Empresa empresa = it.next();
+			
+			if(empresa.getId() == id) {
+				it.remove();
+			}
+		
+		}
+		
+	}
+
+	public Empresa buscaEmpresaPelaId(Integer id) {
+		
+		for (Empresa empresa : lista) {
+			if(empresa.getId() == id) {
+				return empresa;
+			}
+		}
+		
+		return null;
 	}
 
 }
